807d83c8ead34062299e25d416c260d321ddbabe
abarg12 <aidan.barg@gmail.com>
2023-04-04 11:07:45 -0400

    check for python version 2.7

---
+++ PortScan.py
@@ -0,0 +1,12 @@
+import sys
+
+if sys.version_info[0] != 2 or sys.version_info[1] != 7:
+    print("This script requires Python version 2.7")
+    sys.exit(1)
+
+
+
+
+
+if __name__ == "__main__":
+    print("hello, world")
